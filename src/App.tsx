f72b85c3ff24f92c9ea680242ce3c84d4c2287b2
saintabomination <saint.abomination@protonmail.com>
2022-04-12 20:56:53 +0200

    Creating Window component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Desktop from './components/Desktop.component';
 import Taskbar from './components/Taskbar.component';
+import Window from './components/Window.component';
 
 const App = (): JSX.Element =>
   (
     <div>
       <Desktop>
         <Taskbar />
+        <Window />
       </Desktop>
     </div>
   );
---
+++ src/components/Window.component.tsx
@@ -0,0 +1,8 @@
+const Window = (): JSX.Element =>
+  (
+    <div className="window-wrap">
+      Window
+    </div>
+  );
+
+export default Window;
